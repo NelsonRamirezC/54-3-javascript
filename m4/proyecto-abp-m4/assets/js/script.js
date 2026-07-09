31717fd45f4bc70c7c86327f0b6558c42d7056ef
Nelson Ramírez <xhyskoax@gmail.com>
2026-07-09 12:06:01 -0400

    Se termina operaciones de CRUD de las tarea

--- m4/proyecto-abp-m4/assets/js/script.js
+++ m4/proyecto-abp-m4/assets/js/script.js
@@ -6,21 +6,33 @@ const crearFilaTabla = (tarea) => {
     let { id, descripcion, estado, fechaCreacion } = tarea;
 
     let estadoTexto = estado ? "Finalizado" : "Pendiente";
+    
+    let estiloBagde = estado ? "text-bg-success" : "text-bg-warning";
     return `
         <tr>
             <th scope="row">${id}</th>
                 <td>${descripcion}</td>
-                <td>${estadoTexto}</td>
+                <td>
+                    <span class="badge ${estiloBagde}">${estadoTexto}</span></td>
                 <td>${fechaCreacion}</td>
                 <td>
-                    <button class="btn btn-warning" data-id="${id}">Cambiar Estado</button>
-                    <button class="btn btn-danger" data-id="${id}">Eliminar</button>
+                    <button class="btn btn-warning btn-cambiar-estado" data-id="${id}">Cambiar Estado</button>
+                    <button class="btn btn-danger btn-eliminar-tarea" data-id="${id}">Eliminar</button>
                 </td>
         </tr> `;
 };
 
 const actualizarTablaTareas = (tareas = []) => {
-    if (tareas.length == 0) return;
+    if (tareas.length == 0) {
+        document.querySelector("#tabla-tareas").classList.add("d-none");
+        document
+            .querySelector("#mensaje-sin-tareas")
+            .classList.remove("d-none");
+        return;
+    }
+
+    document.querySelector("#tabla-tareas").classList.remove("d-none");
+    document.querySelector("#mensaje-sin-tareas").classList.add("d-none");
 
     let acumuladorFilas = "";
     for (const tarea of tareas) {
@@ -32,7 +44,9 @@ const actualizarTablaTareas = (tareas = []) => {
 
 const alternarBotonesCrearTarea = () => {
     const botonCrearTarea = document.getElementById("btn-crear-tarea");
-    const botonCrearTareaSpinner = document.getElementById("btn-crear-tarea-spinner");
+    const botonCrearTareaSpinner = document.getElementById(
+        "btn-crear-tarea-spinner",
+    );
     botonCrearTarea.classList.toggle("d-none");
     botonCrearTareaSpinner.classList.toggle("d-none");
 };
@@ -68,8 +82,62 @@ formCrearTarea.addEventListener("submit", (event) => {
 
             //TERMINANDO DE CREAR EL FORMULARIO LIMPIAMOS EL INPUT
             formCrearTarea.reset();
-        }, 1500);
+        }, 1000);
     } catch (error) {
         console.log(error);
     }
 });
+
+//DELEGACIÓN EVENTO BOTONES EDITAR Y ELIMINAR TAREAS
+
+document
+    .querySelector("#tabla-tareas tbody")
+    .addEventListener("click", (event) => {
+        const elemento = event.target;
+
+        if (elemento.tagName != "BUTTON") return;
+
+        const id = elemento.dataset.id;
+
+        if (elemento.className.includes("btn-cambiar-estado")) {
+            //AQUÍ EDITAMOS EL ESTAMOS
+            let confirmacion = false;
+
+            let tarea = gestorTareas.obtenerTareaPorId(id);
+
+            if (tarea.estado) {
+                confirmacion = confirm(
+                    "Está seguro de remover el estado finalizado y dejar tarea pendiente?",
+                );
+            } else {
+                confirmacion = confirm("Está seguro de finalizar a tarea?");
+            }
+
+            if (confirmacion) {
+                gestorTareas.cambiarEstadoTarea(id);
+            }
+        } else if (elemento.className.includes("btn-eliminar-tarea")) {
+            //AQUÍ ELIMINAMOS LA TAREA
+
+            let confirmacion = confirm(
+                `Está seguro de eliminar la tarea con id: ${id}?`,
+            );
+
+            if (!confirmacion) return;
+
+            let respuesta = gestorTareas.eliminarTarea(id);
+
+            if (respuesta) {
+                alert("Tarea eliminada correctamente");
+            } else {
+                alert(
+                    "Error al intentar eliminar la tarea, vuelva a intentar...",
+                );
+            }
+        }
+
+        // AL FINAL ACTUALIZAR LA TABLA DE TAREAS
+
+        const tareas = gestorTareas.obtenerTareas();
+        actualizarTablaTareas(tareas);
+    });
